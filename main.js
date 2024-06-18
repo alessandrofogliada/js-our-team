b151641a71429ca5b3dcb6ec4cfa58782997f4c5
alessandrofogliada <fogliada.a@gmail.com>
2024-06-18 16:57:53 +0200

    1.0.0 Eseguito Milestone 0 e 1

---
+++ main.js
@@ -0,0 +1,44 @@
+// creare un array di oggetti per rappresentare i membri del team.
+
+
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+
+const membriTeam = [
+        // Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+    {"nomecognome" : "Wayne Barnett",
+        "Ruolo" : "Foundr e CEO" ,
+        "img" : "wayne-barnett-founder-ceo.jpg"
+    },
+
+    {"nomecognome" : "Angela Caroll",
+        "Ruolo" : "Chief Editor" ,
+        "img" : "angela-caroll-chief-editor.jpg"
+    },
+
+    {"nomecognome" : "Walter Gordon",
+        "Ruolo" : "Office Manager" ,
+        "img" : "walter-gordon-office-manager.jpg"
+    },
+
+    {"nomecognome" : "Angela Lopez",
+        "Ruolo" : "Social Media Manager" ,
+        "img" : "angela-lopez-social-media-manager-ceo.jpg"
+    },
+    {"nomecognome" : "Scott Estrada",
+        "Ruolo" : "Developer" ,
+        "img" : "scott-estrada-developer.jpg"
+    },      
+    {"nomecognome" : "Barbara Ramos",
+        "Ruolo" : "Founder e CEO",
+        "img" : "barbara-ramos-founder-ceo.jpg"
+    }       
+];
+
+// MILESTONE 1:
+// Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
+console.log( membriTeam);
+
+// MILESTONE 2:
+// Stampare le stesse informazioni su DOM sottoforma di stringhe
